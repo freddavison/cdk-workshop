4d9981481f0f1d9bd69aeb5b556347a466d55e74
Fred <fred.davison@outlook.com>
2024-05-31 16:26:56 -0400

    :sparkles: complete

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,19 +1,32 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {Code, Function, Runtime} from 'aws-cdk-lib/aws-lambda';
+import {LambdaRestApi} from 'aws-cdk-lib/aws-apigateway';
+import {HitCounter} from './hit-counter';
+import {TableViewer} from 'cdk-dynamo-table-viewer'
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
+    const hello = new Function(this, 'HelloHandler', {
+      runtime: Runtime.NODEJS_LATEST,
+      code: Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    })
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    })
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    new LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler
+    })
+
+    new TableViewer(this, 'ViewHitCounter', {
+      title: 'Hello Hits',
+      table: helloWithCounter.table,
+      sortBy: '-hits'
+    })
   }
 }
